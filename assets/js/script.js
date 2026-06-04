cf40f5b4332b331c7cd88ead1c8d724e69157212
Garo1995 <garikarshakyan3@gmail.com>
2026-06-04 11:23:06 +0400

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 
+$.mask.definitions['h'] = "[0|1|3|4|5|6|7|9]"
+$(".mask-phone").mask("+7 (h99) 999-99-99");
+
+
 $(document).ready(function () {
     $('.open-menu').on('click', function () {
         $(this).toggleClass('close-menu');
